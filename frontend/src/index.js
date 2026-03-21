fa3d1da317aa2714d1ba91c8d1993c7bc578a838
Naresh N <nareshn141819@gmail.com>
2026-03-21 14:39:32 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,7 +10,7 @@ root.render(
   <React.StrictMode>
     <ClerkProvider
       publishableKey={PUBLISHABLE_KEY}
-      afterSignOutUrl="/#/"
+      afterSignOutUrl="/EmotionAdpative-AI/#/auth/"
     >
       <App />
     </ClerkProvider>
